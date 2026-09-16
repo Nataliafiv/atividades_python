c4aa4548f0c6026d872f276db81f2523d2e19569
Nataliafiv <nf40131@gmail.com>
2026-09-16 15:54:59 -0300

    Update atividade_1.py

--- atividade_1.py
+++ atividade_1.py
@@ -1,24 +1,8 @@
 primos = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
+def pesquisa_sequencial(primos, item):
+    for i, j in enumerate(primos):
+        if j == item:
+            return i
 
-def pesquisa_binaria(lista, item):
-    baixo = 0
-    alto = len(lista) - 1
-
-    while baixo <= alto:
-        meio = (baixo + alto) // 2
-        chute = lista[meio]
-
-        if chute == item:
-            return meio
-        elif chute > item:
-            alto = meio - 1
-        else:
-            baixo = meio + 1
-
-    return None
-
-
-posicao = pesquisa_binaria(primos, 67)
-
-print("Quantidade de números primos menores que 67:", posicao)
+print(pesquisa_sequencial(primos, 67))
